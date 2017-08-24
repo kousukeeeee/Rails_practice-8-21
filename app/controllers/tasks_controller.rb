817642a9443acf0e4cdc00cddfd57d505b34869a
Kousuke <kawai.k@tam-tam.co.jp>
2017-08-24 08:25:16 +0000

    fifth

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :title)
   end
   
   def set_params
